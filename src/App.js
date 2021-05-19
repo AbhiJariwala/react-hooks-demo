e515d352dcb0b11e18c283972933d71a2f267b18
Abhishek Jariwala <abhishekjari15@gmail.com>
2021-05-19 22:21:34 +0530

    using useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Fragment, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import MainHeader from "./components/MainHeader/MainHeader";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -7,12 +7,19 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
+    localStorage.setItem('isAuthenticated', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isAuthenticated');
     setIsLoggedIn(false);
   };
+
+  useEffect(() => {
+    console.log('inside use effect');
+    setIsLoggedIn(localStorage.getItem('isAuthenticated') === '1');
+  }, [])
 
   return (
     <Fragment>
