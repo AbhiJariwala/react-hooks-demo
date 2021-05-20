4b610d6d5863bd09dcb556060b6e5f09bf652c42
Abhishek Jariwala <abhishekjari15@gmail.com>
2021-05-20 22:13:31 +0530

    using react context api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import {Fragment, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import MainHeader from "./components/MainHeader/MainHeader";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -22,13 +23,13 @@ function App() {
   }, [])
 
   return (
-    <Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
+      <MainHeader onLogout={logoutHandler}/>
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!isLoggedIn && <Login onLogin={loginHandler}/>}
+        {isLoggedIn && <Home onLogout={logoutHandler}/>}
       </main>
-    </Fragment>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
+  /* using auth context consume to use context values */
+  /*return (
+    <AuthContext.Consumer>
+      {(authContext) => {
+        return (
+          <nav className={classes.nav}>
+            <ul>
+              {authContext.isLoggedIn && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {authContext.isLoggedIn && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {authContext.isLoggedIn && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        )
+      }}
+    </AuthContext.Consumer>
+  );*/
+
+  /* using useContext hook to use context values */
+
+  const authContext = useContext(AuthContext);
+
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {authContext.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {authContext.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {authContext.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
